d7483ea57ab36772d685afb45dad5631404c78e8
halu828 <halu7colors@gmail.com>
2014-07-17 04:54:54 +0900

    0015: National Budget by catupper 途中

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -1,13 +1,44 @@
-﻿#define _CRT_SECURE_NO_WARNINGS // scanf()の警告文に対しての解決策
+﻿// http://www.atelier-blue.com/contest/pc-concours/2004-y/2004y01-18.htm
+// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=502606
+
+#define _CRT_SECURE_NO_WARNINGS // scanf()の警告文に対しての解決策
 #include <stdio.h>
 
 int main() {
-	int i, d, s;
+	int i, j, n;
+	int tmp, carry = 0;
+	int alen, blen;
+	char a[82], b[82], out[82];
 	
-	while (scanf("%d", &d) != EOF) {
-		s = 0;
-		for (i = 1; i*d < 600; i++) s += (i*d)*(i*d)*d;
-		printf("%d\n", s);
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%s", a);
+		scanf("%s", b);
+		alen = strlen(a);
+		blen = strlen(b);
+		alen--;
+		blen--;
+
+		for (j = 80; j >= 0; j--) {
+			tmp = carry;
+			if (alen >= 0) tmp += a[alen--] - '0';
+			if (blen >= 0) tmp += b[blen--] - '0';
+			out[j] = tmp % 10 + '0';
+			carry = tmp / 10;
+		}
+		if (out[0] != '0') {
+			printf("overflow\n");
+		}
+		else {
+			out[81] = '\0';
+			for (j = 0;; j++) {
+				if (out[j] != '0') {
+					printf("%s\n", &out[j]);
+					break;
+				}
+			}
+		}
+
 	}
 	return 0;
 }

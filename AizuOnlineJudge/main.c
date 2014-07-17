a855bb070a76298f3dd9be8627cef03469a60c5e
halu828 <halu7colors@gmail.com>
2014-07-18 06:13:08 +0900

    0015: National Budget by catupper 途中2

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -1,44 +1,45 @@
 ﻿// http://www.atelier-blue.com/contest/pc-concours/2004-y/2004y01-18.htm
 // http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=502606
+// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=587902
+// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=874199 ← これ強い
+// コメントアウト Ctrl + K + C
+// 解除 Ctrl + K + U
 
 #define _CRT_SECURE_NO_WARNINGS // scanf()の警告文に対しての解決策
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	int i, j, n;
-	int tmp, carry = 0;
-	int alen, blen;
-	char a[82], b[82], out[82];
-	
+	int n, i, alen, blen, carry, out[80];
+	char a[80], b[80];
+
 	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
+	while (n--) {
 		scanf("%s", a);
 		scanf("%s", b);
 		alen = strlen(a);
 		blen = strlen(b);
-		alen--;
-		blen--;
-
-		for (j = 80; j >= 0; j--) {
-			tmp = carry;
-			if (alen >= 0) tmp += a[alen--] - '0';
-			if (blen >= 0) tmp += b[blen--] - '0';
-			out[j] = tmp % 10 + '0';
-			carry = tmp / 10;
-		}
-		if (out[0] != '0') {
+		if (alen > 80 || blen > 80){
 			printf("overflow\n");
+			continue;
 		}
-		else {
-			out[81] = '\0';
-			for (j = 0;; j++) {
-				if (out[j] != '0') {
-					printf("%s\n", &out[j]);
-					break;
-				}
-			}
+		i = carry = 0;
+		while (alen || blen) {
+			out[i] = carry;
+			if (alen) out[i] += a[--alen] - '0';
+			if (blen) out[i] += b[--blen] - '0';
+			carry = out[i] / 10;
+			out[i++] %= 10;
 		}
-
+		if (carry) out[i] = 1;
+		else i--;
+		if (i >= 80) {
+			printf("overflow\n");
+			continue;
+		}
+		for (; i >= 0; i--)
+			printf("%d", out[i]);
+		printf("\n");
 	}
 	return 0;
 }

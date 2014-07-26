ff44818ebe009ec2699668ae5921171a2ca96813
halu828 <halu7colors@gmail.com>
2014-07-26 16:31:31 +0900

    0017: Caesar Cipher

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -1,45 +1,26 @@
-﻿// http://www.atelier-blue.com/contest/pc-concours/2004-y/2004y01-18.htm
-// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=502606
-// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=587902
-// http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=874199 ← これ強い
-// コメントアウト Ctrl + K + C
+﻿// コメントアウト Ctrl + K + C
 // 解除 Ctrl + K + U
 
 #define _CRT_SECURE_NO_WARNINGS // scanf()の警告文に対しての解決策
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
-	int n, i, alen, blen, carry, out[80];
-	char a[80], b[80];
+	char buf[82];
+	int i, j;
 
-	scanf("%d", &n);
-	while (n--) {
-		scanf("%s", a);
-		scanf("%s", b);
-		alen = strlen(a);
-		blen = strlen(b);
-		if (alen > 80 || blen > 80){
-			printf("overflow\n");
-			continue;
+	while (fgets(buf, 82, stdin) != NULL) {
+		for (i = 0; i < 26; i++) {
+			for (j = 0; j < strlen(buf); j++) {
+				if (buf[j] == 'z') buf[j] = 'a';
+				else if (isalpha(buf[j])) buf[j]++;
+			}
+			if (strstr(buf, "the") != NULL ||
+				strstr(buf, "this") != NULL ||
+				strstr(buf, "that") != NULL)
+				printf("%s", buf);
 		}
-		i = carry = 0;
-		while (alen || blen) {
-			out[i] = carry;
-			if (alen) out[i] += a[--alen] - '0';
-			if (blen) out[i] += b[--blen] - '0';
-			carry = out[i] / 10;
-			out[i++] %= 10;
-		}
-		if (carry) out[i] = 1;
-		else i--;
-		if (i >= 80) {
-			printf("overflow\n");
-			continue;
-		}
-		for (; i >= 0; i--)
-			printf("%d", out[i]);
-		printf("\n");
 	}
 	return 0;
 }

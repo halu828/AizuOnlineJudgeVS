4292bacc41570062f3ce26c8ee67c31235089f74
halu828 <halu7colors@gmail.com>
2014-07-27 14:47:59 +0900

    0021: Parallelism 途中

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -3,16 +3,17 @@
 
 #define _CRT_SECURE_NO_WARNINGS // scanf()の警告文に対しての解決策
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
-int main() {
-	char buf[512];
-	int i;
-
-	gets(buf);
-	for (i = 0; i < strlen(buf); i++)
-		buf[i] = toupper(buf[i]);
-	printf("%s\n", buf);
+int main(){
+	int i, n;
+	double x1, y1, x2, y2, x3, y3, x4, y4, sl1, sl2;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++){
+		scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3, &x4, &y4);
+		sl1 = (y2 - y1) / (x2 - x1);
+		sl2 = (y4 - y3) / (x4 - x3);
+		if (sl1 == sl2)	puts("YES");
+		else puts("NO");
+	}
 	return 0;
 }

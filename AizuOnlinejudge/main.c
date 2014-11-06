1264941524ca05a6eb0081eb0fff5446052f9f9f
halu828 <halu7colors@gmail.com>
2014-11-06 09:30:32 +0900

    0024: Physical Experiments

--- AizuOnlinejudge/main.c
+++ AizuOnlinejudge/main.c
@@ -1,19 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS // scanf()‚ÌŒx•¶‚É‘Î‚µ‚Ä‚Ì‰ğŒˆô
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-	int a[4], b[4], i, j, hit, blow;
-	while (scanf("%d %d %d %d", &a[0], &a[1], &a[2], &a[3]) != EOF) {
-		scanf("%d %d %d %d", &b[0], &b[1], &b[2], &b[3]);
-		hit = 0;
-		blow = 0;
-		for (i = 0; i < 4; i++) {
-			if (a[i] == b[i]) hit++;
-			for (j = 0; j < 4; j++) {
-				if (a[i] == b[j] && a[i] != b[i]) blow++;
-			}
-		}
-		printf("%d %d\n", hit, blow);
+	double v, t, y;
+	while (scanf("%lf", &v) != EOF) {
+		printf("%d\n", (int)ceil((4.9 * (v / 9.8) * (v / 9.8) + 5.0) / 5.0));
 	}
 	return 0;
 }
